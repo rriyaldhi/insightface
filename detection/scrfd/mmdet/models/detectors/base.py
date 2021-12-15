084bdd723161ff1604db8adfc3632188fd0a839a
rriyaldhi <rriyaldhi@gmail.com>
2021-12-15 10:36:20 +0000

    test imshow_det_bboxes

--- detection/scrfd/mmdet/models/detectors/base.py
+++ detection/scrfd/mmdet/models/detectors/base.py
@@ -411,8 +411,8 @@ def imshow_det_bboxes(img,
         bbox_int = bbox.astype(np.int32)
         left_top = (bbox_int[0], bbox_int[1])
         right_bottom = (bbox_int[2], bbox_int[3])
-        cv2.rectangle(
-            img, left_top, right_bottom, bbox_color, thickness=thickness)
+#         cv2.rectangle(
+#             img, left_top, right_bottom, bbox_color, thickness=thickness)
         # label_text = class_names[
         #     label] if class_names is not None else f'cls {label}'
         # if len(bbox) > 4:
